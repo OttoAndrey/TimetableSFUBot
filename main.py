b2e4011a3706bc9c332b76ef852222ce827411fc
Ray <ottotruepro@gmail.com>
2019-02-11 17:49:29 +0700

    new

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import requests
-import json
 from flask import Flask
 from flask import request
 from flask import jsonify
 from flask_sslify import SSLify
+from bs4 import BeautifulSoup
 
 token = '649594459:AAH1hu90_ZrEhAtZehVDL9xYvkiE2MnLvhU'
 URL = 'https://api.telegram.org/bot' + token + '/'
@@ -11,6 +11,23 @@ proxies = {'https': 'http://193.85.228.180:	36247'}
 
 app = Flask(__name__)
 sslify = SSLify(app)
+
+# ////////////////////////
+URL_of_group = ""
+
+def get_html(url):
+    r = requests.get(url)
+    return r.text
+
+def get_timetable_week(html):
+    timetable = ''
+    soup = BeautifulSoup(html, 'lxml')
+    trows = soup.find('table', class_='table timetable').find_all('tr', class_='table-center')
+    for row in trows:
+        timetable += row.text + '\n'
+
+    return timetable
+# ////////////////////////////
 
 def send_message(chat_id, text='wait, please'):
     url = URL + 'sendMessage'
@@ -25,11 +42,19 @@ def index():
         chat_id = r['message']['chat']['id']
         message = r['message']['text']
 
+        if message == 'Ки15-17б':
+            URL_of_group = 'http://edu.sfu-kras.ru/timetable?group=КИ15-17б'
+        elif message == 'Сб18-11б':
+            URL_of_group = 'http://edu.sfu-kras.ru/timetable?group=СБ18+-+11Б'
+
+        # ответы на номер группы
         if len(message) != 0:
             send_message(chat_id, message)
+        else:
+            send_message(chat_id, get_timetable_week(get_html(URL_of_group)))
 
         return jsonify(r)
-    return 'nothing'
+    return 'kek'
 
 if __name__ == '__main__':
     app.run()
