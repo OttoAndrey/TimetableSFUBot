99bfc58bd339a4fb7703c6ba63b6e21be3b3f9ea
Ray <ottotruepro@gmail.com>
2019-04-03 10:10:50 +0700

    расписание по типам недели (чет, нечет)

--- main.py
+++ main.py
@@ -17,22 +17,22 @@ sslify = SSLify(app)
 # ////////////////////////
 
 def get_html(url):
-    html = requests.get(url[:-1])
+    html = requests.get(url)
     return html.text
 
 
-def get_group_url(user_message):
+def get_group_url(number_of_group):
     f = open('text')
     group_url = 'Не удалось найти расписание'
     for line in f:
-        if user_message.lower() in line:
-            group_url = 'http://edu.sfu-kras.ru/timetable' + line[line.find('?'):]
+        if number_of_group.lower() in line:
+            group_url = 'http://edu.sfu-kras.ru/timetable' + line[line.find('?'):-1]
             break
     f.close()
     return group_url
 
 
-def get_timetable_week(html):
+def get_timetable_week(html, type='текущая неделя'):
     soup = BeautifulSoup(html, 'lxml')
 
     type_of_week = soup.find('div', class_='content').find('p').find('b').text[5:]
@@ -41,32 +41,101 @@ def get_timetable_week(html):
     table = soup.find('table', class_='table timetable')
     week = ''
 
-    for row in table:
-        if hasattr(row.find('th', colspan=4), 'text'):
-            week += '{0}/{1}'.format(row.find('th', colspan=4).text.upper(), type_of_week) + '\n'
+    if type == 'текущая неделя':
+        # текущая неделя
+        for row in table:
+            if hasattr(row.find('th', colspan=4), 'text'):
+                week += '\n'
+                week += '{0}/{1}/{2}'.format(row.find('th', colspan=4).text.upper(), type_of_week, type) + '\n'
 
-        if hasattr(row.find('td', width='1%'), 'text'):
-            if hasattr(row.find('td', class_='nobr'), 'text'):
-                if hasattr(row.find('td', class_='light', width='40%').find('b'), 'text'):
-                    if hasattr(row.find('td', class_='light', width='40%'), 'contents'):
-                        if hasattr(row.find('td', class_='light', width='40%').find('em'), 'text'):
-                            week += '{0} {1} {2}{3} /{4}/ Аудитория: {5}'.format(row.find('td', width='1%').text,
-                                                                                 row.find('td', class_='nobr').text,
-                                                                                 row.find('td', class_='light',
-                                                                                          width='40%').find('b').text,
-                                                                                 row.find('td', class_='light',
-                                                                                          width='40%').contents[1],
-                                                                                 row.find('td', class_='light',
-                                                                                          width='40%').find('em').text,
-                                                                                 row.find('td', class_='light',
-                                                                                          width='40%').find(
-                                                                                     'b').findNextSibling(
-                                                                                     'a').text) + '\n'
-                        else:
-                            week += '{0} {1} {2}'.format(row.find('td', width='1%').text,
-                                                         row.find('td', class_='nobr').text,
-                                                         row.find('td', class_='light', width='40%').find(
-                                                             'b').text) + '\n'
+            if hasattr(row.find('td', width='1%'), 'text'):
+                if hasattr(row.find('td', class_='nobr'), 'text'):
+                    if hasattr(row.find('td', class_='light', width='40%').find('b'), 'text'):
+                        if hasattr(row.find('td', class_='light', width='40%'), 'contents'):
+                            if hasattr(row.find('td', class_='light', width='40%').find('em'), 'text'):
+                                week += '{0} {1} {2}{3} /{4}/ Аудитория: {5}'.format(row.find('td', width='1%').text,
+                                                                                     row.find('td', class_='nobr').text,
+                                                                                     row.find('td', class_='light',
+                                                                                              width='40%').find(
+                                                                                         'b').text,
+                                                                                     row.find('td', class_='light',
+                                                                                              width='40%').contents[1],
+                                                                                     row.find('td', class_='light',
+                                                                                              width='40%').find(
+                                                                                         'em').text,
+                                                                                     row.find('td', class_='light',
+                                                                                              width='40%').find(
+                                                                                         'b').findNextSibling(
+                                                                                         'a').text) + '\n'
+                            else:
+                                week += '{0} {1} {2}'.format(row.find('td', width='1%').text,
+                                                             row.find('td', class_='nobr').text,
+                                                             row.find('td', class_='light', width='40%').find(
+                                                                 'b').text) + '\n'
+
+    elif type in ['нечет', 'нечетная', 'нечетная неделя', '1']:
+        # нечетная неделя
+        for row in table:
+            if hasattr(row.find('th', colspan=4), 'text'):
+                week += '\n'
+                week += '{0}/{1}'.format(row.find('th', colspan=4).text.upper(), 'нечетная неделя') + '\n'
+
+            if hasattr(row.find('td', width='1%'), 'text'):
+                if hasattr(row.find('td', class_='nobr'), 'text'):
+                    if hasattr(row.find('td', width='40%').find('b'), 'text'):
+                        if hasattr(row.find('td', width='40%'), 'contents'):
+                            if hasattr(row.find('td', width='40%').find('em'), 'text'):
+                                week += '{0} {1} {2}{3} /{4}/ Аудитория: {5}'.format(row.find('td', width='1%').text,
+                                                                                     row.find('td', class_='nobr').text,
+                                                                                     row.find('td',
+                                                                                              width='40%').find(
+                                                                                         'b').text,
+                                                                                     row.find('td',
+                                                                                              width='40%').contents[1],
+                                                                                     row.find('td',
+                                                                                              width='40%').find(
+                                                                                         'em').text,
+                                                                                     row.find('td',
+                                                                                              width='40%').find(
+                                                                                         'b').findNextSibling(
+                                                                                         'a').text) + '\n'
+                            else:
+                                week += '{0} {1} {2}'.format(row.find('td', width='1%').text,
+                                                             row.find('td', class_='nobr').text,
+                                                             row.find('td', width='40%').find(
+                                                                 'b').text) + '\n'
+    elif type in ['чет', 'четная', 'четная неделя', '2']:
+        # четная неделя
+        for row in table:
+            if hasattr(row.find('th', colspan=4), 'text'):
+                week += '\n'
+                week += '{0}/{1}'.format(row.find('th', colspan=4).text.upper(), 'четная неделя') + '\n'
+
+            if hasattr(row.find('td', width='1%'), 'text'):
+                if hasattr(row.find('td', class_='nobr'), 'text'):
+                    if hasattr(row.find('td', width='40%').find('b'), 'text'):
+                        a = row.find('td', width='40%').nextSibling
+
+                        if a is None:
+                            a = row.find('td', width='40%')
+
+                        elif a.find('b') is None:
+                            continue
+
+                        if hasattr(row.find('td', width='40%'), 'contents'):
+                            if hasattr(row.find('td', width='40%').find('em'), 'text'):
+                                week += '{0} {1} {2}{3} /{4}/ Аудитория: {5}'.format(row.find('td', width='1%').text,
+                                                                                     row.find('td', class_='nobr').text,
+                                                                                     a.find('b').text,
+                                                                                     a.contents[1],
+                                                                                     a.find('em').text,
+                                                                                     a.find('b').findNextSibling(
+                                                                                         'a').text) + '\n'
+
+                            else:
+                                week += '{0} {1} {2}'.format(row.find('td', width='1%').text,
+                                                             row.find('td', class_='nobr').text,
+                                                             a.find('b').text) + '\n'
 
     return week
 
@@ -130,30 +199,30 @@ def user_massages_handler(chat_id, message):
                 'пт': 'пятница',
                 'сб': 'суббота'}
 
+    types_of_week = ['нечет', 'нечетная', 'нечетная неделя', '1', 'чет', 'четная', 'четная неделя', '2']
+
+    # начало
     if message == '/start':
         send_message(chat_id, 'тут должно быть вступление, но его пока нет')
+
+    # день недели
     elif ' ' in message:
         start = message.index(' ')
         number_of_group = message[:start]
-        day = message[start + 1:]
-        temp = day
-        for key, value in dic_days.items():
-            if day == key:
-                day = value.title()
-                break
-            elif day == value:
-                day = value.title()
-                break
-        # вообще сначала надо проверять на наличие группы, а не потом. кому этот день нужен, если группы нет...
-        if day == temp:
-            send_message(chat_id, 'Некорректный день')
-        elif day != temp:
-            group_url = get_group_url(number_of_group)
-            if group_url == 'Не удалось найти расписание':
-                send_message(chat_id, group_url)
-            elif group_url != 'Не удалось найти расписание':
-                send_message(chat_id, get_timetable_day(get_html(group_url), day))
+        second_part = message[start + 1:]
 
+        # неправильный запрос
+        if get_group_url(number_of_group) == 'Не удалось найти расписание':
+            send_message(chat_id)
+        else:
+            # расписание на день
+            if second_part in dic_days or dic_days.values():
+                send_message(chat_id, get_timetable_day(get_html(get_group_url(number_of_group)), second_part))
+            # расписание на определенную неделю
+            elif second_part in types_of_week:
+                send_message(chat_id, get_timetable_week(get_html(get_group_url(number_of_group)), second_part))
+
+    # неделя текущая
     else:
         group_url = get_group_url(message)
         if group_url == 'Не удалось найти расписание':
